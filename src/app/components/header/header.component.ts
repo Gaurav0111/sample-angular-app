22144f332ba81ab9e140cabfce848d959ce76c61
GauravGarwal <garwalgaurav480@gmail.com>
2025-05-02 11:15:49 +0530

    updated filters and register component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,23 +1,53 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, HttpClientModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  registrationNumber?: ''
   showLoginPopup = false;
   showCallPopup = false;
-  cities = ['Delhi', 'Mumbai', 'Bangalore', 'Chennai', 'Hyderabad', 'Kolkata', 'Pune', 'Ahmedabad', 'Jaipur', 'Surat'];
+  allCities: any[] | undefined
+  Cities = ['Delhi', 'Mumbai', 'Bangalore', 'Chennai', 'Hyderabad', 'Kolkata', 'Pune', 'Ahmedabad', 'Jaipur', 'Surat'];
+  stateNames: any[] | undefined;
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+  ) { 
+    this.http.get<any[]>('https://raw.githubusercontent.com/Gaurav0111/sample-angular-app/main/src/assets/cities.json')
+    .subscribe({
+      next: data => {
+        this.allCities = data.filter(city => city.country_code === 'IN');
+        this.stateNames = [...new Set(this.allCities.map(city => city.state_name))].sort()
+        // .slice(0, 15)
+        ;
+      },
+      error: err => {
+        console.error('Error fetching cities:', err);
+      }
+    });
+  }
+  
   showProfile() {
-    this.showLoginPopup =!this.showLoginPopup;
+    this.showLoginPopup = !this.showLoginPopup;
     this.showCallPopup = false;
   }
   showCall() {
-    this.showLoginPopup =false;
+    this.showLoginPopup = false;
     this.showCallPopup = !this.showCallPopup;
   }
+  registrationSubmit() {
+    console.log("number: ", this.registrationNumber);
+    this.router.navigate(['/register-vehicle'], {
+      state: { Number: this.registrationNumber }
+    });
+  }
+  
 }
--- src/app/components/register-vehicle/register-vehicle.component.ts
+++ src/app/components/register-vehicle/register-vehicle.component.ts
@@ -18,7 +18,9 @@ export class RegisterVehicleComponent {
   vehicleForm: FormGroup;
   currentStep = 1;
   manufacturingYears: number[] = [];
-  availableLocations: string[] = [];
+  availableStates: string[] = [];
+  allCities: { state_name: string; name: string; country_code: string }[] = [];
+  citiesForSelectedState: string[] = [];
   fuelTypes = ['Petrol', 'Diesel', 'Electric', 'CNG'];
   transmissions = ['Automatic', 'Manual'];
 
@@ -33,26 +35,35 @@ export class RegisterVehicleComponent {
     for (let y = currentYear; y >= currentYear - 25; y--) {
       this.manufacturingYears.push(y);
     }
-    // this.http.get<any[]>('assets/cities.json').subscribe(data => {
-      this.http.get<any[]>('https://raw.githubusercontent.com/Gaurav0111/sample-angular-app/main/src/assets/cities.json').subscribe(data => {
-        this.availableLocations = data
-          .filter(city => city.country_code === 'IN')
-          .map(city => city.name)
-          .sort();
+
+    this.http.get<any[]>('https://raw.githubusercontent.com/Gaurav0111/sample-angular-app/main/src/assets/cities.json')
+      .subscribe(data => {
+        this.allCities = data.filter(city => city.country_code === 'IN');
+        this.availableStates = [...new Set(this.allCities.map(city => city.state_name))].sort();
       });
-      
 
     this.vehicleForm = this.fb.group({
-      registrationNumber: [this.registrationNumber, [Validators.required,Validators.pattern(/^[A-Z]{2} ?\d{1,3} ?[A-Z]{1,3} ?\d{4}$/i),],],
-      locations: ['', Validators.required],
+      registrationNumber: [this.registrationNumber, [Validators.required, Validators.pattern(/^[A-Z]{2} ?\d{1,3} ?[A-Z]{1,3} ?\d{4}$/i)]],
+      state: ['', Validators.required],
+      city: ['', Validators.required],
       brand: ['', Validators.required],
       fuelType: ['', Validators.required],
       transmission: ['', Validators.required],
-      manufacturingYear: ['', [Validators.required]],
+      manufacturingYear: ['', Validators.required],
       carModel: ['', Validators.required],
       kilometersDriven: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
       planningToSell: ['', Validators.required],
       phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+    });
+
+    this.vehicleForm.get('state')?.valueChanges.subscribe(selectedState => {
+      this.citiesForSelectedState = this.allCities
+        .filter(city => city.state_name === selectedState)
+        .map(city => city.name)
+        .sort();
+
+      // Reset city when state changes
+      this.vehicleForm.get('city')?.setValue('');
     });
   }
 
@@ -69,14 +80,15 @@ export class RegisterVehicleComponent {
   }
 
   isStep1Valid() {
-    return this.vehicleForm.get('locations')?.valid &&
+    return this.vehicleForm.get('state')?.valid &&
+      this.vehicleForm.get('city')?.valid &&
       this.vehicleForm.get('brand')?.valid &&
-      this.vehicleForm.get('manufacturingYear')?.valid &&
       this.vehicleForm.get('fuelType')?.valid;
-  }
-
-  isStep2Valid() {
-    return this.vehicleForm.get('carModel')?.valid &&
+    }
+    
+    isStep2Valid() {
+      return this.vehicleForm.get('carModel')?.valid &&
+      this.vehicleForm.get('manufacturingYear')?.valid &&
       this.vehicleForm.get('kilometersDriven')?.valid &&
       this.vehicleForm.get('planningToSell')?.valid &&
       this.vehicleForm.get('transmission')?.valid;
